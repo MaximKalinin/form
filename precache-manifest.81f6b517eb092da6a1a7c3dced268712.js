263878322a1f7d369135c946b4c2adb7bfc120ae
Maksim <maxim.kalinin.work@gmail.com>
2019-03-02 15:38:50 +0300

    Updates

---
+++ precache-manifest.81f6b517eb092da6a1a7c3dced268712.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6a5ae8d0c994b8fb09bc6c8cbc73642",
+    "url": "/form/static/media/winter.b6a5ae8d.jpg"
+  },
+  {
+    "revision": "8f83450ae5ac05f3116f849fb2389061",
+    "url": "/form/static/media/summer.8f83450a.jpg"
+  },
+  {
+    "revision": "a72c3db161b2d376001e26942cc92f5d",
+    "url": "/form/static/media/spring.a72c3db1.jpg"
+  },
+  {
+    "revision": "e6ff995773507e7acee714b4404b2081",
+    "url": "/form/static/media/fall.e6ff9957.jpg"
+  },
+  {
+    "revision": "b1e9ddaa9aae0b8452f5",
+    "url": "/form/static/js/runtime~main.b1e9ddaa.js"
+  },
+  {
+    "revision": "ab9b92580b39cd68be22",
+    "url": "/form/static/js/main.ab9b9258.chunk.js"
+  },
+  {
+    "revision": "07bd6ff83068ca315b0a",
+    "url": "/form/static/js/2.07bd6ff8.chunk.js"
+  },
+  {
+    "revision": "ab9b92580b39cd68be22",
+    "url": "/form/static/css/main.04eff2a9.chunk.css"
+  },
+  {
+    "revision": "660c8a131d513e65ff0962b29ce1c55c",
+    "url": "/form/index.html"
+  }
+];
